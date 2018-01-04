c7d8b06ce85737809c5704a59b51ea990e2ec96d
Hui Zhang <hvzhang@gmail.com>
2018-01-04 22:08:28 +0800

    Initial commit

---
+++ src/main/java/cn/ah/springcloud/controller/ConsumerController.java
@@ -0,0 +1,35 @@
+package cn.ah.springcloud.controller;
+
+import cn.ah.springcloud.model.User;
+import cn.ah.springcloud.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/12/29.
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value="/feign-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return helloService.hello();
+    }
+
+
+    @GetMapping(value = "/feign-consumer2")
+    public String helloConsumer2(){
+        StringBuffer sb =new StringBuffer();
+        sb.append(helloService.hello()).append("\n");
+        sb.append(helloService.hello("didi")).append("\n");
+        sb.append(helloService.hello("didi", 30)).append("\n");
+        sb.append(helloService.hello(new User("username", 23))).append("\n");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/cn/ah/springcloud/service/HelloService.java
@@ -0,0 +1,28 @@
+package cn.ah.springcloud.service;
+
+import cn.ah.springcloud.model.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Administrator on 2017/12/29.
+ */
+@Component
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("/hello")
+    String hello();
+
+    @GetMapping(value = "/hello1")
+    String hello(@RequestParam("name") String name);
+
+    @GetMapping(value = "/hello2")
+    String hello(@RequestHeader("name") String name,
+                 @RequestHeader("age") Integer age);
+
+    @PostMapping(value = "/hello3")
+    String hello(@RequestBody User user);
+
+}
